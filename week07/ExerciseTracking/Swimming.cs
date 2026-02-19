5b460a1f9b883af13aec540e9a7782ece29dd096
SchmuckeDj <schjosepu@gmail.com>
2026-02-19 18:20:29 -0400

    week07 tracking

---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Swimming : Activity
+{
+    private int _laps;
+    private const double LapDistance = 0.05; // 50 meters = 0.05 km
+
+    public Swimming(string date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * LapDistance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
